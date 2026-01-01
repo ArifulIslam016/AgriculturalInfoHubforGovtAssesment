cc322d27fd24d8cf725a706d1e2744157a34176a
Ariful <arifulq234@gmail.com>
2026-01-01 12:32:14 +0600

    saved commit

--- src/Components/CropdetailsforHomePage.jsx
+++ src/Components/CropdetailsforHomePage.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import cropsData from './CropData';
-import CropCard from './CropCard';
+import React from "react";
+import cropsData from "./CropData";
+import CropCard from "./CropCard";
 
 const CropdetailsforHomePage = () => {
-    const CropsDta=cropsData
- const sortData=cropsData.slice(0,4)
-    return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
-                    <h1 className="text-green-600 text-3xl text-center">Crop Details</h1>
+  const CropsDta = cropsData;
+  const sortData = cropsData.slice(0, 4);
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+      <h1 className="text-green-600 text-3xl text-center">Crop Details</h1>
 
-            {sortData.map((crop,index)=><CropCard key={index} crop={crop}></CropCard>)}
-        </div>
-    );
+      {sortData.map((crop, index) => (
+        <CropCard key={index} crop={crop}></CropCard>
+      ))}
+    </div>
+  );
 };
 
 export default CropdetailsforHomePage;
